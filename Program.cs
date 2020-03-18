fd8c91c0f281db3f157ee2f7c06c84e072e3b436
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-03-18 18:59:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Enquiry_Form_Using_Properties
+{
+    class Program
+    {
+        private string _name,_gender,_fathername,_mothername,_pincode,_contactno,_email,_coursename,_doj;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender= value; }
+        }
+        public string Fathername
+        {
+            get { return _fathername; }
+            set { _fathername = value; }
+        }
+
+        public string Mothername
+        {
+            get { return _mothername; }
+            set { _mothername = value; }
+        }
+         public string Contact
+        {
+            get { return _contactno; }
+            set { _contactno = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string CourseName
+        {
+            get { return _coursename; }
+            set { _coursename = value; }
+        }
+        public string DateofJoining
+        {
+            get { return _doj; }
+            set { _doj = value; }
+        }
+    }
+}
---
+++ printing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enquiry_Form_Using_Properties
+{
+    class printing
+    {
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.WriteLine("***********Enquiry Form Using Properties***********");
+
+            Console.Write("Enter Name:");     //Name
+            p.Name = Console.ReadLine();
+            
+            Console.Write("Enter Gender:");   //Gender
+            p.Gender = Console.ReadLine();
+            
+            Console.Write("Enter Father Name:"); //FatherName
+            p.Fathername = Console.ReadLine();
+            
+            Console.Write("Enter Mother Name:");   //MotherName
+            p.Mothername = Console.ReadLine();
+
+            Console.Write("Enter Contact Number:");  //ContactNumber
+            p.Contact = Console.ReadLine();
+
+            Console.Write("Enter Email Id:");    //Email Id
+            p.Email = Console.ReadLine();
+
+            Console.Write("Enter Course Name:"); //CourseName
+            p.CourseName = Console.ReadLine();
+
+            Console.Write("Enter Date Of Joining:"); //Date Of Joining
+            p.DateofJoining = Console.ReadLine();
+
+            Console.Write("\n");
+            Console.WriteLine("**************Invoice**************");
+            Console.WriteLine("Registration No:1\t Date: "+ DateTime.Now.ToString("dd MMMM yyyy hh:mm tt"));
+            Console.WriteLine("Received with thanks from Mr." + p.Name);
+            Console.WriteLine("The Sum of 'Rs.5000' in words 'Five Thousand Rupees Only /-'");
+
+
+
+        }
+    }
+}
